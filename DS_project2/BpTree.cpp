b7801fa06fb3063922da83fd3c750cf978f48ec0
rkddk7165 <rkddk7165@naver.com>
2023-11-06 16:30:25 +0900

    초기 커밋.

---
+++ DS_project2/BpTree.cpp
@@ -0,0 +1,41 @@
+#include "BpTree.h"
+#include <string>
+
+using namespace std;
+
+bool BpTree::Insert(LoanBookData* newData) {
+
+	return true;
+}
+
+bool BpTree::excessDataNode(BpTreeNode* pDataNode) {
+	if (pDataNode->getDataMap()->size() > order - 1) return true;//order is equal to the number of elements 
+	else return false;
+}
+
+bool BpTree::excessIndexNode(BpTreeNode* pIndexNode) {
+	if (pIndexNode->getIndexMap()->size() > order - 1)return true;//order is equal to the number of elements 
+	else return false;
+}
+
+void BpTree::splitDataNode(BpTreeNode* pDataNode) {
+
+}
+
+void BpTree::splitIndexNode(BpTreeNode* pIndexNode) {
+
+}
+
+BpTreeNode* BpTree::searchDataNode(string name) {
+	BpTreeNode* pCur = root;
+
+	return pCur;
+}
+
+bool BpTree::searchBook(string name) {
+
+}
+
+bool BpTree::searchRange(string start, string end) {
+
+}
